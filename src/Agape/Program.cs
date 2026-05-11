6bd4a4a1201a52f20f0ac58c3c031f766820ae34
Wakunguma K. <contact@wakunguma.com>
2026-05-11 14:09:44 -0600

    Initial commit

---
+++ src/Agape/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+using Silk.NET.Maths;
+using Silk.NET.Windowing;
+
+namespace Agape;
+
+class Widget
+{
+    
+}
+
+static class Program
+{
+
+	private static IWindow _window;
+
+    static void Main()
+    {
+		
+		var windowOptions = WindowOptions.Default with
+		{
+			Title = "Agape",
+			Size = new Vector2D<int>(500,500),	
+			API = new GraphicsAPI(ContextAPI.OpenGL,new APIVersion(3,3))
+		};
+		_window = Window.Create(windowOptions);
+		_window.Run();
+        Console.WriteLine("Hello, World!");
+    }
+}
