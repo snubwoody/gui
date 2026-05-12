5d75d7c69ff11d23d7a060b58d93c2455e15f8c7
Wakunguma K. <contact@wakunguma.com>
2026-05-12 17:16:12 -0600

    Move App class to App.cs

---
+++ src/Agape/App.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+
+namespace Agape;
+
+class App
+{
+    private readonly IWindow _window;
+    private static GL _gl;
+
+    /// <summary>
+    /// Instantiates a new app.
+    /// </summary>
+    public App()
+    {
+        var windowOptions = WindowOptions.Default with
+        {
+            Title = "Agape",
+            Size = new Vector2D<int>(500,500),	
+            API = new GraphicsAPI(ContextAPI.OpenGL,new APIVersion(3,3))
+        };
+        _window = Window.Create(windowOptions);
+        _window.Load += OnLoad;
+        _window.Render += OnRender;
+    }
+
+    private void OnLoad()
+    {
+        _gl = _window.CreateOpenGL();
+        _gl.ClearColor(Color.White);
+    }
+
+    private void OnRender(double delta)
+    {
+        _gl.Clear(ClearBufferMask.ColorBufferBit);
+    }
+
+    public void Run()
+    {
+        _window.Run();
+    }
+}
--- src/Agape/Program.cs
+++ src/Agape/Program.cs
@@ -1,59 +1,14 @@
-﻿using System.Drawing;
-using System.Numerics;
-using Silk.NET.Maths;
-using Silk.NET.OpenGL;
-using Silk.NET.Windowing;
-
-namespace Agape;
+﻿namespace Agape;
 
 class Widget
 {
     
 }
 
-class App
-{
-	private readonly IWindow _window;
-	private static GL _gl;
-
-	/// <summary>
-	/// Instantiates a new app.
-	/// </summary>
-	public App()
-	{
-		var windowOptions = WindowOptions.Default with
-		{
-			Title = "Agape",
-			Size = new Vector2D<int>(500,500),	
-			API = new GraphicsAPI(ContextAPI.OpenGL,new APIVersion(3,3))
-		};
-		_window = Window.Create(windowOptions);
-		_window.Load += OnLoad;
-		_window.Render += OnRender;
-	}
-
-	private void OnLoad()
-	{
-		_gl = _window.CreateOpenGL();
-		_gl.ClearColor(Color.White);
-	}
-
-	private void OnRender(double delta)
-	{
-		_gl.Clear(ClearBufferMask.ColorBufferBit);
-	}
-
-	public void Run()
-	{
-		_window.Run();
-	}
-}
-
 static class Program
 {
     static void Main()
     {
-		
 	    var app  = new App();
 	    app.Run();
     }
