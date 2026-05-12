334e3aefb04f9ddca493681b9ab17991de98cc93
Wakunguma K. <contact@wakunguma.com>
2026-05-12 17:13:51 -0600

    Clear screen on load

--- src/Agape/Program.cs
+++ src/Agape/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Numerics;
+﻿using System.Drawing;
+using System.Numerics;
 using Silk.NET.Maths;
+using Silk.NET.OpenGL;
 using Silk.NET.Windowing;
 
 namespace Agape;
@@ -9,14 +11,16 @@ class Widget
     
 }
 
-static class Program
+class App
 {
+	private readonly IWindow _window;
+	private static GL _gl;
 
-	private static IWindow _window;
-
-    static void Main()
-    {
-		
+	/// <summary>
+	/// Instantiates a new app.
+	/// </summary>
+	public App()
+	{
 		var windowOptions = WindowOptions.Default with
 		{
 			Title = "Agape",
@@ -24,7 +28,33 @@ static class Program
 			API = new GraphicsAPI(ContextAPI.OpenGL,new APIVersion(3,3))
 		};
 		_window = Window.Create(windowOptions);
+		_window.Load += OnLoad;
+		_window.Render += OnRender;
+	}
+
+	private void OnLoad()
+	{
+		_gl = _window.CreateOpenGL();
+		_gl.ClearColor(Color.White);
+	}
+
+	private void OnRender(double delta)
+	{
+		_gl.Clear(ClearBufferMask.ColorBufferBit);
+	}
+
+	public void Run()
+	{
 		_window.Run();
-        Console.WriteLine("Hello, World!");
+	}
+}
+
+static class Program
+{
+    static void Main()
+    {
+		
+	    var app  = new App();
+	    app.Run();
     }
 }
