010f2f9748071265210449cc3dd69074b51254a6
Wakunguma K. <contact@wakunguma.com>
2026-05-12 19:58:47 -0600

    Add SingleChildWidth and EmptyWidget

--- src/Agape/Widget.cs
+++ src/Agape/Widget.cs
@@ -10,12 +10,12 @@ public abstract record BoxSizing
     /// <summary>
     /// Fits it children, i.e. as small as possible.
     /// </summary>
-    public sealed record Fit : BoxSizing;
+    public sealed record Shrink : BoxSizing;
     /// <summary>
     /// Fills the available width.
     /// </summary>
     /// <param name="Factor"></param>
-    public sealed record Flex(double Factor) : BoxSizing;
+    public sealed record Fill(double Factor) : BoxSizing;
 }
 
 public record BoxConstraints
@@ -47,32 +47,11 @@ public record BoxConstraints
 
 public abstract class Widget
 {
-    public BoxSizing IntrinsicWidth { get; init; }
-    public BoxSizing IntrinsicHeight { get; init; }
+    public BoxSizing IntrinsicWidth { get; init; } = new BoxSizing.Shrink();
+    public BoxSizing IntrinsicHeight { get; init; } = new BoxSizing.Shrink();
     public BoxConstraints Constraints { get; init; } = new();
-    /// <summary>
-    /// The minimum width a widget is allowed to be, takes precedence over all values.  
-    /// </summary>
-    public double? MinWidth { get; init; }
-    /// <summary>
-    /// The minimum width a height is allowed to be, takes precedence over all values.  
-    /// </summary>
-    public double? MinHeight { get; init; }
 
-    public void SolveMinConstraints()
-    {
-        // The minimum width and height take precedence if set
-        if (IntrinsicWidth is BoxSizing.Fixed value && !Constraints.MinimumWidth.HasValue)
-        {
-            Constraints.MinimumWidth = value.Value;
-        }
-
-        if (IntrinsicHeight is BoxSizing.Fixed fixedHeight && !Constraints.MinimumHeight.HasValue)
-        {
-            Constraints.MinimumHeight = fixedHeight.Value;
-        }
-
-    }
+    public abstract void SolveMinConstraints();
 
     /// <summary>
     /// Draw the widget to the skia canvas.
@@ -80,7 +59,102 @@ public abstract class Widget
     public abstract void Draw(SKCanvas canvas);
 }
 
-public class Rect : Widget
+/// <summary>
+/// A widget with no children.
+/// </summary>
+public abstract class EmptyWidget : Widget
+{
+    public override void SolveMinConstraints()
+    {
+        // The minimum width and height take precedence if set
+        if (IntrinsicWidth is BoxSizing.Fixed width && !Constraints.MinimumWidth.HasValue)
+        {
+            Constraints.MinimumWidth = width.Value;
+        }
+
+        if (IntrinsicHeight is BoxSizing.Fixed height && !Constraints.MinimumHeight.HasValue)
+        {
+            Constraints.MinimumHeight = height.Value;
+        }
+    }
+}
+
+/// <summary>
+/// A widget with one child.
+/// </summary>
+public abstract class SingleChildWidget : Widget
+{
+    protected Widget _child;
+
+    public SingleChildWidget(Widget child)
+    {
+        _child = child;
+    }
+    
+    public override void SolveMinConstraints()
+    {
+        // The minimum width and height take precedence if set
+        if (IntrinsicWidth is BoxSizing.Fixed width && !Constraints.MinimumWidth.HasValue)
+        {
+            Constraints.MinimumWidth = width.Value;
+        }
+
+        if (IntrinsicHeight is BoxSizing.Fixed height && !Constraints.MinimumHeight.HasValue)
+        {
+            Constraints.MinimumHeight = height.Value;
+        }
+    }
+}
+
+public class Container : SingleChildWidget
+{
+    public Vector2 Size { get; set; }
+    public SKColor Color { get; set; }
+
+    public Container(Widget child) : base(child){}
+
+    public override void SolveMinConstraints()
+    {
+        _child.SolveMinConstraints();
+        if (!Constraints.MinimumWidth.HasValue)
+        {
+            if (IntrinsicWidth is BoxSizing.Fixed width)
+            {
+                Constraints.MinimumWidth = width.Value;
+            }
+            else
+            {
+                Constraints.MinimumWidth = _child.Constraints.MinimumWidth;
+            }
+        }
+
+        if (!Constraints.MinimumHeight.HasValue)
+        {
+            if (IntrinsicHeight is BoxSizing.Fixed height)
+            {
+                Constraints.MinimumHeight = height.Value;
+            }
+            else
+            {
+                Constraints.MinimumHeight = _child.Constraints.MinimumHeight;
+            }
+        }
+    }
+
+    public override void Draw(SKCanvas canvas)
+    {
+
+        var rect = SKRect.Create(80, 80, Size.X, Size.Y);
+        var paint = new SKPaint
+        {
+            Color = Color,
+        };
+        canvas.DrawRect(rect, paint);
+        _child.Draw(canvas);
+    }
+}
+
+public class Rect : EmptyWidget
 {
     public Vector2 Size { get; set; }
     public SKColor Color { get; set; }
--- tests/Agape.Tests/LayoutTests.cs
+++ tests/Agape.Tests/LayoutTests.cs
@@ -30,4 +30,68 @@ public class LayoutTests
         Assert.Equal(200, rect.Constraints.MinimumWidth);
         Assert.Equal(100, rect.Constraints.MinimumHeight);
     }
+
+    [Fact]
+    public void EmptyWidgetMinWidthAndHeight()
+    {
+        var rect = new Rect
+        {
+            Constraints = new BoxConstraints(minHeight: 100, minWidth: 200),
+        };
+
+        rect.SolveMinConstraints();
+        Assert.Equal(200, rect.Constraints.MinimumWidth);
+        Assert.Equal(100, rect.Constraints.MinimumHeight);
+    }
+
+    [Fact]
+    public void SingleChildWidgetMinWidthAndHeight()
+    {
+        var rect = new Rect();
+
+        var widget = new Container(rect)
+        {
+            Constraints = new BoxConstraints(minHeight: 100, minWidth: 200),
+        };
+
+        widget.SolveMinConstraints();
+        Assert.Equal(200, widget.Constraints.MinimumWidth);
+        Assert.Equal(100, widget.Constraints.MinimumHeight);
+    }
+
+    [Fact]
+    public void SingleChildWidgetFixedMinConstraints()
+    {
+        var rect = new Rect();
+
+        var widget = new Container(rect)
+        {
+            IntrinsicWidth = new BoxSizing.Fixed(200),
+            IntrinsicHeight = new BoxSizing.Fixed(150),
+        };
+
+        widget.SolveMinConstraints();
+        Assert.Equal(200, widget.Constraints.MinimumWidth);
+        Assert.Equal(150, widget.Constraints.MinimumHeight);
+    }
+
+    [Fact]
+    public void SingleChildWidgetShrinkMinConstraints()
+    {
+        var rect = new Rect
+        {
+            IntrinsicWidth = new BoxSizing.Fixed(20.5),
+            IntrinsicHeight = new BoxSizing.Fixed(10),
+        };
+
+        var widget = new Container(rect)
+        {
+            IntrinsicWidth = new BoxSizing.Shrink(),
+            IntrinsicHeight = new BoxSizing.Shrink()
+        };
+
+        widget.SolveMinConstraints();
+        Assert.Equal(20.5, widget.Constraints.MinimumWidth);
+        Assert.Equal(10, widget.Constraints.MinimumHeight);
+    }
 }
